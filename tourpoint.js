c86da18c2911867fd8cf1d10459a497eec9b070a
Tunjira330 <158020809+Tunjira330@users.noreply.github.com>
2024-05-13 16:44:25 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai Nation Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50326574484723,
+            14.332883369880761
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49161552077248,
+            13.751103488377026
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Suvarnabhumi ",
+          "level": 5,
+          "type": "space"
+        },
+        "geometry": {
+          "coordinates": [
+            100.75024477116324,
+            13.689881374125008
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ko Phi Phi Leh",
+          "type": "naturl",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.76627849999784,
+            7.6845610234167765
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Koh Kood Beach ",
+          "type": "naturl",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            102.53443658465471,
+            11.667481056247794
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Hua Hin Beach",
+          "level": 4,
+          "type": "naturl"
+        },
+        "geometry": {
+          "coordinates": [
+            99.96393551722412,
+            12.555521739583796
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phra Kaew",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49258549999945,
+            13.751811795511614
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Yai Si Suphan",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.48842499999813,
+            13.729944516524142
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดสาริกา ตรัง",
+          "type": "cutural",
+          "level": 2
+        },
+        "geometry": {
+          "coordinates": [
+            99.61041191534491,
+            7.532821689154318
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ko Kradat",
+          "type": "naturl",
+          "level": 2
+        },
+        "geometry": {
+          "coordinates": [
+            102.52103275227859,
+            11.833284548672083
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
